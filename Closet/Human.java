4c126a08e88f8b053bd369c62e6e71e6d2d7af0e
gnewman00 <gnewman@stuy.edu>
2017-01-12 08:55:14 -0500

    Created a skeleton for the Human class.

---
+++ Closet/Human.java
@@ -0,0 +1,20 @@
+public class Human extends Player {
+
+    private String name;    // instance var
+
+    public Human() { // constructor
+	// insert implentation here 
+    }
+
+    public String getName(){ // accessor
+	return name; }
+
+    public void setName(String newName){ // mutator
+	name = newName;
+    }
+	
+    public static void main(String[] args){
+    }
+
+
+}
